24961c4ef98b938b7c9392b2c0fd5d791f65e3ca
JD <jdstep23@gmail.com>
2015-05-31 15:56:06 -0700

    fixed formatting

--- d3-loop.js
+++ d3-loop.js
@@ -1,14 +1,14 @@
 var square = d3.select('.container').selectAll('div')
-            .data([0]).enter().append('div').attr('class', 'square')
-            .style({
-              // top:    0 + 'px',
-              // left:   0 + 'px',
-              'background-color': 'blue',
-              top: 15 + 'px',
-              left: 15 + 'px',
-              width:  100 + 'px',
-              height: 100 + 'px'
-            });
+  .data([0]).enter().append('div').attr('class', 'square')
+  .style({
+    // top:    0 + 'px',
+    // left:   0 + 'px',
+    'background-color': 'blue',
+    top: 15 + 'px',
+    left: 15 + 'px',
+    width:  100 + 'px',
+    height: 100 + 'px'
+});
 
 var turnSquareGreen = function() {
   square.transition()
@@ -19,7 +19,7 @@ var turnSquareGreen = function() {
     .each('end', function() {
       turnSquareBlue();
     });
-}
+};
 
 var turnSquareBlue = function () {
   square.transition()
@@ -30,6 +30,6 @@ var turnSquareBlue = function () {
     .each('end', function() {
       turnSquareGreen();
     });
-}
+};
 
 turnSquareGreen();
