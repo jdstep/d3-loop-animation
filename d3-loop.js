8d167c2934dadfac00fc7d6ddceb4c020a2ff263
Jonathan Davis <jdstep23@gmail.com>
2015-06-14 15:47:25 -0700

    added comments to d3 code for clarity

--- d3-loop.js
+++ d3-loop.js
@@ -1,6 +1,7 @@
+// creates a square for us to play with
 var square = d3.select('.container').selectAll('div')
   .data([0]).enter().append('div').attr('class', 'square')
-  .style({
+  .style({ // sets CSS styles
     'background-color': 'blue',
     top: 15 + 'px',
     left: 15 + 'px',
@@ -8,6 +9,7 @@ var square = d3.select('.container').selectAll('div')
     height: 100 + 'px'
 });
 
+// turns our square green by using an animation on css properties
 var turnSquareGreen = function() {
   square.transition()
     .duration(1000)
@@ -19,6 +21,7 @@ var turnSquareGreen = function() {
     });
 };
 
+// turns our square blue by using an animation on css properties
 var turnSquareBlue = function () {
   square.transition()
     .duration(1000)
@@ -30,4 +33,5 @@ var turnSquareBlue = function () {
     });
 };
 
+// starts our infinite loop of chaniging the square colors
 turnSquareGreen();
